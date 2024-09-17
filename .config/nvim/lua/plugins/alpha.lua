fbba2f5c728f9374c08b07c728021930431189f0
Angel Campoverde <angel.campoverde@cern.ch>
2024-09-17 08:13:47 +0800

    Adding working config for neovim

---
+++ .config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,24 @@
+return {
+	"goolord/alpha-nvim",
+	event = "VimEnter",
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		-- Set menu
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "N > New File", "<cmd>ene<CR>"),
+			dashboard.button("Leader ee", "T > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("Leader ff", "F > Find File", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("Leader fs", "W > Find Word", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("Leader wr", "R > Restore Session", "<cmd>SessionRestore<CR>"),
+			dashboard.button("q", "Q > Quit NVIM", "<cmd>qa<CR>"),
+		}
+
+		-- Send config to alpha
+		alpha.setup(dashboard.opts)
+
+		-- Disable folding on alpha buffer
+		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+	end,
+}
