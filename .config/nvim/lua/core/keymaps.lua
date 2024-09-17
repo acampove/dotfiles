dd487d26cb7b326a4bb640451bc43f56abb80999
Angel Campoverde <angel.campoverde@cern.ch>
2024-09-17 12:37:49 +0800

    Better keybindings
    Adding bindins to maximize and minimize

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -3,26 +3,44 @@ vim.g.mapleader = "\\"
 local km = vim.keymap -- for conciseness
 
 km.set("i", "jk", "<ESC>")
+
+-- Move line up or down
 km.set("n", "+", "ddkkp")
 km.set("n", "-", "ddp")
+
+-- Hide highlight
 km.set("n", "<leader>c", ":noh<cr>")
-km.set("n", "<leader>g", ":Git<cr>")
+
+-- Remove trailing spaces
+km.set("n", "<leader>s", ":%s/\\s\\+$//<cr>")
+
+-- Go to last file
 km.set("n", "<leader>l", ":e #<cr>")
+
 km.set("n", "<leader>p", "<c-w>p ")
 km.set("n", "<leader>q", ":q<cr>")
 km.set("n", "<leader>r", ":so $MYVIMRC<cr>")
-km.set("n", "<leader>s", ":%s/\\s\\+$//<cr>")
 km.set("n", "<leader>w", ":w<cr>")
-km.set("n", "<leader>z", ":Lazy<cr>")
 
 -- Map arrow keys to switch panels 
-km.set("n", "<Up>"   , "<C-W>j")
-km.set("n", "<Down>" , "<C-W>k")
+km.set("n", "<Up>"   , "<C-W>k")
+km.set("n", "<Down>" , "<C-W>j")
 km.set("n", "<Right>", "<C-W>l")
 km.set("n", "<Left>" , "<C-W>h")
+
+-- Maximize and minimize splits
+km.set("n", "<leader>a" , "<C-W>_")
+km.set("n", "<leader>s" , "<C-W>=")
+
+-- Lazy
+km.set("n", "<leader>z", ":Lazy<cr>")
 
 -- Telescope
 km.set("n", "<leader>ff", ":Telescope find_files<cr>", {})
 km.set("n", "<leader>fg", ":Telescope live_grep<cr>" , {})
 km.set("n", "<leader>fb", ":Telescope buffers<cr>"   , {})
 km.set("n", "<leader>fh", ":Telescope help_tags<cr>" , {})
+
+-- Neogit 
+km.set("n", "<leader>g" , ":Neogit kind=split_below<cr>" , {})
+
