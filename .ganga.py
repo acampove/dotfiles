9083d2b26fc82de453d066d748b66e2c51626437
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-13 11:48:52 +0800

    Removing not needed imports

--- .ganga.py
+++ .ganga.py
@@ -18,9 +18,8 @@ sys.path.append('/cvmfs/lhcb.cern.ch/lib/var/lib/LbEnv/3386/stable/linux-64/lib/
 import tqdm
 from dmu.logging.log_store import LogStore
 
-from GangaCore.GPI             import Job, Executable, DiracFile, LocalFile, Interactive, GenericSplitter, Dirac, Local, File
+from GangaCore.GPI                             import DiracFile 
 from GangaCore.GPIDev.Lib.Registry.JobRegistry import JobRegistrySliceProxy as JRSP
-
 
 log = LogStore.add_logger('_ganga_py')
 # ----------------------------------
