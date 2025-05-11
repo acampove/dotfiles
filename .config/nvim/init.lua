238887d3f98c0223c3eb1027490aa693bd06855f
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-11 19:36:18 +0800

    Adding lines needed to enable warning text
    This broke, not sure why

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,3 +8,10 @@ vim.cmd [[highlight SpellRare  cterm=underline ctermfg=yellow guifg=yellow]]
 vim.cmd [[highlight SpellLocal cterm=underline ctermfg=green  guifg=green ]]
 
 vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+vim.diagnostic.config({
+  virtual_text     = true,  -- re-enable inline messages
+  signs            = true,
+  underline        = true,
+  update_in_insert = false,
+})
