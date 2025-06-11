c5c35a284bf84dff3923c3da3bf20303a1b64d4e
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-11 20:15:11 +0800

    Adding file to configure mason-lspconfig separetely from mason itself

---
+++ .config/nvim/lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,30 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    local mason_lspconfig      = require("mason-lspconfig")
+    local mason_tool_installer = require("mason-tool-installer")
+
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "clangd",
+        "bashls",
+        "jsonls",
+      },
+      automatic_installation = false,
+      automatic_enable = true,
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua",   -- lua formatter
+        "isort",    -- python formatter
+        "pylint",
+      },
+    })
+  end,
+}
