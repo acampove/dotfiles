65bd67bc2adc523f2a81a31f4cb2e2b8d82c1cd6
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-11 20:28:34 +0800

    Remove DiagnosticsSign lines, this is deprecated

--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -83,13 +83,10 @@ return {
     -- used to enable autocompletion (assign to every lsp server config)
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    -- Change the Diagnostic symbols in the sign column (gutter)
-    -- (not in youtube nvim video)
-    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-    end
+    vim.diagnostic.config({
+        virtual_text = true,
+        signs        = true,
+        underline    = false,
+    })
     end,
 }
-
